61ff98d51752b3624142497d63e718efb7fa415b
cheeseNA <kurocat2000@gmail.com>
2022-03-11 16:43:58 +0900

    Implement setTimerStatus on background.js

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("onInstalled fired");
+  chrome.storage.sync.set({
+    timing: { hour: 0, minute: 0 },
+    count: 0,
+    timerStatus: "not set",
+  });
+});
+
 chrome.tabs.onActivated.addListener((activeInfo) => {
   console.log("onActivated fired");
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -29,4 +38,26 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-function setTimerStatus() {}
+function setTimerStatus(lang) {
+  chrome.storage.sync.get(["timerStatus"], function (statusResult) {
+    const timerStatus = statusResult.timerStatus;
+    console.log(`timer status: ${timerStatus}`);
+    if (lang === "en" && timerStatus === "not set") {
+      console.log("start timer");
+      const intervalId = setInterval(() => {
+        chrome.storage.sync.get(["count"], function (countResult) {
+          const nowCount = countResult.count;
+          console.log(`now count is ${nowCount}`);
+          chrome.storage.sync.set({ count: nowCount + 1 });
+          chrome.action.setBadgeText({ text: nowCount.toString() });
+        });
+      }, 1000);
+      chrome.storage.sync.set({ timerStatus: intervalId });
+    }
+    if (lang !== "en" && timerStatus !== "not set") {
+      console.log("stop timer");
+      clearInterval(timerStatus);
+      chrome.storage.sync.set({ timerStatus: "not set" });
+    }
+  });
+}
