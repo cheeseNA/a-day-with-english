396ac342c9ae3b950ae6a1f615708b9301c4c427
cheeseNA <kurocat2000@gmail.com>
2022-03-14 19:14:55 +0900

    Add onFocusChanged listener to detect a change of focus window

--- background.js
+++ background.js
@@ -7,6 +7,32 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+
+chrome.windows.onFocusChanged.addListener((windowId) => {
+  console.log("onFocusChanged fired");
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    console.log("no focus");
+    setTimerStatus("no focus");
+  } else {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length !== 1) {
+        console.log("tab.length is not one");
+        return;
+      }
+      const targetTabId = tabs[0].id;
+      chrome.tabs.sendMessage(
+        targetTabId,
+        { type: "requestLang" },
+        (response) => {
+          console.log("received requestLang response");
+          console.log(`lang: ${response.lang}`);
+          setTimerStatus(response.lang);
+        }
+      );
+    });
+  }
+});
+
 chrome.tabs.onActivated.addListener((activeInfo) => {
   console.log("onActivated fired");
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
