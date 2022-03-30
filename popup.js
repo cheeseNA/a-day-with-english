6fe1fdc81b73c469eee69bbe3ef0fa8f85b3eb9f
cheeseNA <kurocat2000@gmail.com>
2022-03-30 22:44:58 +0900

    Add language status message to popup

--- popup.js
+++ popup.js
@@ -1,8 +1,20 @@
+import { getLangResponseFromActiveTab } from "./language-fetcher.js";
+
 document.addEventListener("DOMContentLoaded", () => {
+  asyncWrapperForPopupEvent();
+});
+
+async function asyncWrapperForPopupEvent() {
   const statusDiv = document.getElementById("siteStatus");
   const domainButton = document.getElementById("domainButton");
   const urlButton = document.getElementById("urlButton");
   const textSpanElement = document.createElement("span");
-  textSpanElement.textContent = `This page is written in ???`;
   statusDiv.appendChild(textSpanElement);
-});
+  const response = await getLangResponseFromActiveTab();
+  if (response === "fail" || !response) {
+    textSpanElement.textContent =
+      "The extension cannot fetch the language of this page";
+  } else {
+    textSpanElement.textContent = `This page is written in '${response.lang}'`;
+  }
+}
